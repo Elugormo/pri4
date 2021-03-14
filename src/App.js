7fa77a6c60b506445154664bef2cf28a2bad041f
Ivan <romanomess@gmail.com>
2021-03-14 12:04:32 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { ChessBoard } from "./ChessBoard";
 import { Header } from "./Header";
+import { Container } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+
+const styles = {
+  display: "flex",
+  justifyContent: "center",
+  marginTop: "30px",
+};
 
 let positions = [
   [
@@ -110,7 +118,7 @@ let positions = [
   ],
   [
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/br.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wn.png",
     },
     {
       src: null,
@@ -136,16 +144,16 @@ let positions = [
   ],
   [
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/br.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wp.png",
     },
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/br.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wp.png",
     },
     {
       src: null,
     },
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/br.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wb.png",
     },
     {
       src: null,
@@ -168,27 +176,27 @@ let positions = [
       src: null,
     },
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/br.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wp.png",
     },
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/br.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wb.png",
     },
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/br.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wn.png",
     },
     {
       src: null,
     },
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/br.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wp.png",
     },
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/br.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wp.png",
     },
   ],
   [
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/br.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wr.png",
     },
     {
       src: null,
@@ -197,7 +205,7 @@ let positions = [
       src: null,
     },
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/br.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wq.png",
     },
     {
       src: null,
@@ -206,7 +214,7 @@ let positions = [
       src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wr.png",
     },
     {
-      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wr.png",
+      src: "https://images.chesscomfiles.com/chess-themes/pieces/neo/80/wk.png",
     },
     {
       src: null,
@@ -219,6 +227,23 @@ function App() {
     <div className="App">
       <Header />
       <ChessBoard positions={positions} />
+      <Container style={styles}>
+        <Nav defaultActiveKey="/home" as="ul">
+          <Nav.Item as="li">
+            <Nav.Link href="https://www.chess.com/learn-how-to-play-chess">
+              How to play chess
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link
+              href="https://www.chess.com/puzzles/problem/42342"
+              eventKey="link-1"
+            >
+              Chess Problem
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
     </div>
   );
 }
--- src/ChessBoard.js
+++ src/ChessBoard.js
@@ -1,21 +1,28 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
-
+import "./ChessBoard.css";
 export const ChessBoard = ({ positions }) => {
   return (
     <Container>
-      <Table style={{ width: "unset", margin: "15px auto" }}>
+      <Table className="main-container">
         <tbody>
-          {positions.map((row) => (
+          {positions.map((row, idx) => (
             <tr className="row">
+              <th>{idx + 1}</th>
               {row.map((piece, parityIdx) => (
-                <td style={{ width: "100px" }} className="cell">
+                <td className="cell">
                   <img src={piece.src} />
                 </td>
               ))}
             </tr>
           ))}
+          <tr className="row">
+            <td></td>
+            {["a", "b", "c", "d", "e", "f", "g", "h"].map((el) => (
+              <td className="table-data">{el}</td>
+            ))}
+          </tr>
         </tbody>
       </Table>
     </Container>
